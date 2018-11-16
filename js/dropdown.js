e2a1e95480b91aa11f9f007723662ea08b96fde9
Marcelo Bueno <marcelo.silva@mjv.com.br>
2018-11-16 08:20:15 -0200

    teste

---
+++ js/dropdown.js
@@ -0,0 +1,10 @@
+// Dropdown Menu Fade    
+jQuery(document).ready(function () {
+    $(".dropdown").hover(
+        function () {
+            $('.dropdown-menu', this).fadeIn("fast");
+        },
+        function () {
+            $('.dropdown-menu', this).fadeOut("fast");
+        });
+});
---
+++ js/map.js
@@ -0,0 +1,12 @@
+    function initMap() {
+        // Styles a map in night mode.
+        var map = new google.maps.Map(document.getElementById('map'), {
+            center: {
+                lat: 40.674,
+                lng: -73.945
+            },
+            zoom: 12,
+            styles: [
+            ]
+        });
+    } 
