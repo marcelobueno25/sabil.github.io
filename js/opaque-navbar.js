fdcfafaf30f3f3d04db7343dccd04da6fe49b094
Marcelo Bueno <marcelo.silva@mjv.com.br>
2018-11-21 09:16:42 -0200

    ajustado

--- js/opaque-navbar.js
+++ js/opaque-navbar.js
@@ -1,11 +1,11 @@
-
-$(window).scroll(function() {
-    if(($(this).scrollTop() > 50)||($(this).scrollTop() > 50))  /*height in pixels when the navbar becomes non opaque*/ 
-    {
+$(window).scroll(function () {
+    if (($(this).scrollTop() > 50) || ($(this).scrollTop() > 50)) /*height in pixels when the navbar becomes non opaque*/ {
         $('.opaque-texto').addClass('texto');
         $('.opaque-navbar').addClass('opaque');
+        $('.fa-align-right').addClass('fa');
     } else {
         $('.opaque-texto').removeClass('texto');
         $('.opaque-navbar').removeClass('opaque');
+        $('.fa-align-right').removeClass('fa');
     }
 });
