019e9a00e484c9cf7861b1bb632249bc46eac3af
Marcelo Bueno <marcelobueno25@hotmail.com>
2018-11-22 09:29:21 -0200

    Ajuste FINAL

--- js/dropdown.js
+++ js/dropdown.js
@@ -1,5 +1,5 @@
 $('li.dropdown').hover(function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(500);
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeIn(100);
 }, function () {
-    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(500);
+    $(this).find('.dropdown-menu').stop(true, true).delay(200).fadeOut(100);
 });
